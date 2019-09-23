b2af38680a2a6b6a305968a94c45c8d86aea1793
Price-L <Price.l@husky.neu.edu>
2019-09-23 14:45:39 -0400

    Final Solution to Challenge 1.3_1

--- hw/price_hw1.3_1.py
+++ hw/price_hw1.3_1.py
@@ -18,8 +18,9 @@ from matplotlib import pyplot
 
 g = 9.8            # g acceleration
 mass =  0.01        # mass of the particle
-y0 = 1000000.          # initial position
-y01 = 1000000.
+y0 = 10000.          # initial position
+y0max = 1000000.
+y0min = 10000.
 v0 = 0.            # initial velocity
 vt = 30.           # terminal velocity
 gforce = g*mass
@@ -41,10 +42,6 @@ y_d = [y0]
 v_d = [v0]
 t_d = [0.]
 
-#distance = 1+y/R   # distance dependence of gravitational force
-
-#gforce = g*mass/distance**2
-
 def dist_calc(y0, p_d, p):
         
     y_d = [y0]
@@ -52,10 +49,10 @@ def dist_calc(y0, p_d, p):
     t_d = [0.]
 
     while p_d.y > 0.:
-        dist = 1+p_d.y/R
-        gforce_d = g*mass/dist**2
+        dist = 1.+(y_d[-1]/R)
+        gforce_d = g*mass/(dist**2)
         # k2_d = gforce_d/vt**2
-        fy_d = -gforce_d-k2*p_d.v*abs(p_d.v)
+        fy_d = -gforce_d
         p_d.euler(fy_d, dt)
         y_d.append(p_d.y)
         v_d.append(p_d.v)
@@ -66,43 +63,36 @@ def dist_calc(y0, p_d, p):
     t = [0.]
 
     while p.y > 0.:
-        fy = -gforce-k2*p.v*abs(p.v)
+        fy = -gforce
         p.euler(fy, dt)
         y.append(p.y)
         v.append(p.v)
         t.append(t[-1]+dt)
     
-    diff = abs((v_d[-1]-v[-1])/v[-1])*100
+    diff = abs((v_d[-1]-v[-1])/v[-1])
 
     return (y,v,t,y_d,v_d,t_d,diff)
 
 y,v,t,y_d,v_d,t_d,differ = dist_calc(y0, p_d, p)
 
-#if differ < 0.01:
- #   y01 = y0
-  #  y0 = 2*y0
-   # p_d.y = y0
-   # p.y = y0
-   # y,v,t,y_d,v_d,t_d,differ = dist_calc(y0, p_d, p)
-
 while True:    
+    
+    if (abs(differ - 0.01) < 0.0001) and (differ > 0.01):
+        break
 
-    if differ < 0.01:
-        y01 = y0
-        y0 = 2*y0
-        y0 = (y01+y0)/2
-        p_d.y = y0
-        p.y = y0
+    if (differ < 0.01):
+        y0min = y0
+        y0 = (y0+y0max)/2.
+        p_d = particle(mass, y0, v0)
+        p = particle(mass, y0, v0)
         y,v,t,y_d,v_d,t_d,differ = dist_calc(y0, p_d, p)
 
-    if differ > 0.01:
-        y0 = (y01+y0)/2
-        p_d.y = y0
-        p.y = y0
+    if (differ > 0.01):
+        y0max = y0
+        y0 = (y0min+y0)/2.
+        p_d = particle(mass, y0, v0)
+        p = particle(mass, y0, v0)
         y,v,t,y_d,v_d,t_d,differ = dist_calc(y0, p_d, p)
-        
-        if (differ - 0.01) < 0.0001:
-            break
 
 
 t_data = np.array(t) # we convert the list into a numpy array for plotting
@@ -128,4 +118,4 @@ pyplot.plot(td_data, yd_data, color="#FF0000", ls='-', lw=3)
 pyplot.ylabel('position(m)');
 pyplot.show()
 
-print(y0)
+print("Given the initial height: ", y[0], "The impact velocities will have a ", differ*100., "difference")
